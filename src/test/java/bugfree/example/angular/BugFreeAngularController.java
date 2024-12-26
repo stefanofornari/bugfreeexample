2276e6eea29e6966a064ae6b0b2041218525d8a5
Stefano Fornari <stefano.fornari@gmail.com>
2024-12-26 09:35:24 +0100

    upgraded to use latest xtest version

--- src/test/java/bugfree/example/angular/BugFreeAngularController.java
+++ src/test/java/bugfree/example/angular/BugFreeAngularController.java
@@ -2,7 +2,7 @@
  * <FUNAMBOLCOPYRIGHT>
  * Copyright (C) 2015 Funambol.
  * All Rights Reserved.  No use, copying or distribution of this
- * work may be made except in accordance with a valid license
+ * work may be made except in accodance with a valid license
  * agreement from Funambol.  This notice must be
  * included on all copies, modifications and derivatives of this
  * work.
@@ -17,175 +17,161 @@
  */
 package bugfree.example.angular;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import javax.script.ScriptException;
 import static org.assertj.core.api.BDDAssertions.then;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.ProvideSystemProperty;
 import org.mozilla.javascript.NativeArray;
 import org.mozilla.javascript.NativeObject;
 import ste.xtest.js.BugFreeEnvjs;
 import ste.xtest.js.JSAssertions;
-import ste.xtest.net.StubStreamHandler;
-import ste.xtest.net.StubStreamHandlerFactory;
-import ste.xtest.net.StubURLConnection;
+import ste.xtest.net.HttpClientStubber;
+import ste.xtest.net.StubHttpClient;
 
 /**
  *
  * @author ste
  */
 public class BugFreeAngularController extends BugFreeEnvjs {
-    public BugFreeAngularController() throws ScriptException, IOException {
+    public BugFreeAngularController() throws Exception {
         loadScript("src/main/webapp/phones/controllers.js");
         loadScript("src/test/angular/fake.js");
+        exec("document.baseURI='http://localhost:8080/phones/'");
     }
-    
-    @BeforeClass
-    public static void before_class() throws Exception {
-        URL.setURLStreamHandlerFactory(new StubStreamHandlerFactory());
-    }
-    
+
     @Test
     public void load_index() throws Exception {
         givenURLStubs("phones1.json");
-        
+
         exec("document.location='http://localhost:8080/phones/index.html';");
-        
+
         then(exec("document.title;")).isEqualTo("Phone list");
     }
-    
+
     @Test
     public void load_controller_and_data_from_url() throws Exception {
-        givenFileURLStubs("phones1.json");
-        
+        givenURLStubs("phones1.json");
+
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         JSAssertions.then((NativeArray)exec("scope.phones;")).hasSize(3);
         thenPhoneIs(0, "Nexus S", "Fast just got faster with Nexus S.");
         thenPhoneIs(2, "MOTOROLA XOOM™", "The Next, Next Generation tablet.");
     }
-    
+
     @Test
     public void content_is_dynamic() throws Exception {
-        givenFileURLStubs("phones2.json");
-        
+        givenURLStubs("phones2.json");
+
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         JSAssertions.then((NativeArray)exec("scope.phones;")).hasSize(1);
         thenPhoneIs(0, "Huawei", "Best price/quality ratio");
     }
-    
+
     @Test
     public void set_status_in_case_of_errors() throws Exception {
         //
         // connection error
         //
-        givenFileURLStubsWithError(404); // not found
-        
+        givenURLStubsWithError(404); // not found
+
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         JSAssertions.then((NativeArray)exec("scope.phones;")).isEmpty();
         then(exec("scope.status;")).isEqualTo("error");
-        
+
         //
         // in case of success status becomes "ok"
         //
-        givenFileURLStubs("phones2.json"); 
-        
+        givenURLStubs("phones2.json");
+
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         then(exec("scope.status;")).isEqualTo("ok");
-        
+
         //
         // server error
         //
-        givenFileURLStubsWithError(500); 
-        
+        givenURLStubsWithError(500);
+
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         JSAssertions.then((NativeArray)exec("scope.phones;")).isEmpty();
         then(exec("scope.status;")).isEqualTo("error");
     }
-    
+
     /**
      * The url to retrieve the phone list shall be relative to current document
      * location.
-     * 
-     * @throws Exception 
+     *
+     * @throws Exception
      */
     @Test
     public void dynamic_data_retrieving_url() throws Exception {
-        givenFileURLStubs("phones1.json");
-        
-        System.out.println(StubStreamHandler.URLMap.getMapping());
-        
+        debug(true);
+        givenURLStubs("phones1.json");
+
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         thenPhoneIs(0, "Nexus S", "Fast just got faster with Nexus S.");
     }
-    
+
     @Test
     public void list_of_phones1() throws Exception {
         givenURLStubs("phones1.json");
- 
+
         exec("document.location='http://localhost:8080/phones/index.html';");
         exec("controller('PhoneListCtrl', {$scope: scope});");
-        
+
         then(exec("$('ul li:first span').text()")).isEqualTo("Nexus S");
         then(exec("$('ul li:first p').text()")).isEqualTo("Fast just got faster with Nexus S.");
     }
-    
+
     @Test
     public void list_of_phones2() throws Exception {
         givenURLStubs("server.com", "phones2.json");
- 
+
         exec("document.location='http://server.com/phones/index.html';");
         exec("controller('PhoneListCtrl', {$scope: scope});");
 
         then(exec("$('ul li:first span').text()")).isEqualTo("Huawei");
         then(exec("$('ul li:first p').text()")).isEqualTo("Best price/quality ratio");
     }
-    
+
     // --------------------------------------------------------- private methods
-    
+
     private void givenURLStubs(final String server, final String file) throws Exception {
-        StubURLConnection[] builders = prepareUrlStupBuilders(
+        final HttpClientStubber stubber = httpStubber();
+        stubber.stubs().clear();
+        stubber.withStub(
             "http://" + server +"/phones/index.html",
-            "http://" + server + "/phones/phones.json"
+            new StubHttpClient.StubHttpResponse()
+                .file("src/main/webapp/phones/index.html").contentType("text/html")
         );
-        int i=0;
-        builders[i++].file("src/main/webapp/phones/index.html").type("text/html");
-        builders[i++].file("src/test/angular/" + file).type("application/json");
+        stubber.withStub(
+            "http://" + server + "/phones/phones.json",
+            new StubHttpClient.StubHttpResponse()
+                .file("src/test/angular/" + file).contentType("application/json")
+        );
     }
-    
+
     private void givenURLStubs(final String file) throws Exception {
         givenURLStubs("localhost:8080", file);
     }
-    
-    private void givenFileURLStubs(final String file) throws Exception {
-        StubURLConnection[] builders = prepareUrlStupBuilders(
-            "file://" + new File("phones.json").getAbsolutePath()
+
+    private void givenURLStubsWithError(int status) throws Exception {
+        final HttpClientStubber stubber = httpStubber();
+        stubber.stubs().clear();
+        stubber.withStub(
+            "http://localhost:8080/phones/phones.json",
+            new StubHttpClient.StubHttpResponse()
+                .text("not found").statusCode(status)
         );
-        
-        builders[0].file("src/test/angular/" + file).type("application/json");
     }
-    
-    private void givenFileURLStubsWithError(int status) throws Exception {
-        StubURLConnection[] builders = prepareUrlStupBuilders(
-            "file://" + new File("phones.json").getAbsolutePath()
-        );
-        builders[0].text("some text").status(status);
-    }
-    
+
     private void thenPhoneIs(int index, final String phone, final String snippet)
     throws Exception {
         NativeObject p = (NativeObject)exec("scope.phones[" + index + "];");
         then(p.get("name", null)).isEqualTo(phone);
         then(p.get("snippet", null)).isEqualTo(snippet);
     }
-    
 }
